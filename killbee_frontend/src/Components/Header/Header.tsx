3e95f65556e4436731b148215a74d874797018c1
SerhatAkar <serhat.akar@viacesi.fr>
2021-09-21 14:56:22 +0200

    Features : Generic table + toolBar.

--- killbee_frontend/src/Components/Header/Header.tsx
+++ killbee_frontend/src/Components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
