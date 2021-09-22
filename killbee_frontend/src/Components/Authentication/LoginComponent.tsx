fb33872cc207a748f8192bc32618ba65889d9643
SerhatAkar <serhat.akar@viacesi.fr>
2021-09-22 14:26:05 +0200

    Feat : Login button leads to a new dialog with login form (no logic).

---
+++ killbee_frontend/src/Components/Authentication/LoginComponent.tsx
@@ -0,0 +1,129 @@
+import {Button, Dialog, DialogContent, Grid, TextField, Typography, withStyles} from "@material-ui/core";
+import {green, grey, teal} from "@material-ui/core/colors";
+import React, {Component, useState} from "react";
+
+
+const styles = () => ({
+    root: {
+        flexGrow: 1,
+    },
+    primaryColor: {
+        color: teal[500]
+    },
+    secondaryColor: {
+        color: grey[700]
+    },
+    padding: {
+        padding: 4
+    },
+    mainHeader: {
+        backgroundColor: grey[100],
+        padding: 20,
+        alignItems: "center",
+        borderColor: green[200],
+        borderBottomWidth: 3
+    },
+    mainContent: {
+        padding: 40,
+
+    },
+    secondaryContainer: {
+        backgroundColor: grey[200],
+    },
+    inputContainer: {
+        paddingTop: "3vh",
+    },
+    inputFields: {
+        paddingBottom: "4vh",
+        minWidth: "97vh"
+    },
+    loginButton: {
+        paddingBottom: "4vh"
+    }
+});
+
+function LoginComponent(props: any) {
+
+    const {classes} = props;
+    const initForm = {
+        email: "",
+        password: ""
+    };
+    const [form, setForm] = useState(initForm);
+
+    function handleChange(e: any) {
+        const target = e.target;
+        const setName = target.name;
+        const value = target.value;
+        setForm({
+            ...form,
+            [setName]: value
+        })
+        console.log(form);
+    }
+
+    const submitForm = () => {
+        // Mettre le state action ici.
+    }
+
+    const [open, setOpen] = useState<boolean>(true);
+
+    return (
+        <Grid>
+            <Dialog
+                fullWidth
+                maxWidth="md"
+                open={open}
+            >
+                <DialogContent className={classes.padding}>
+                    <Grid item xs={12}>
+                        <Grid item xs={12}>
+                            <Grid container direction="row" xs={12} className={classes.mainHeader}>
+                                <Grid item xs={12}>
+                                    <Typography className={classes.primaryColor} variant="h5">
+                                        Login to Killerbee </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid container xs={12} className={classes.inputContainer}>
+                            <form onSubmit={submitForm}>
+                                <Grid item xs={12} className={classes.inputFields}>
+                                    <TextField
+                                        style={{marginBottom: 20}}
+                                        fullWidth
+                                        margin="dense"
+                                        variant="outlined"
+                                        label="Username"
+                                        defaultValue=""
+                                        placeholder="Username"
+                                    />
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="password"
+                                        type={"password"}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Grid container xs={12} direction={"row"} justifyContent={"center"}
+                                          className={classes.loginButton}>
+                                        <Grid item>
+                                            <Button type="button" variant="outlined" color="primary"
+                                                    className="form__custom-button">
+                                                Log in
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+            </Dialog>
+        </Grid>
+    );
+}
+
+export default withStyles(styles)(LoginComponent);
--- killbee_frontend/src/Views/WelcomeView.tsx
+++ killbee_frontend/src/Views/WelcomeView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Button,
     Grid,
@@ -7,14 +7,12 @@ import {
 } from "@material-ui/core";
 
 import {createMuiTheme, makeStyles} from '@material-ui/core/styles';
-
 import ManagementList from '../Components/ManagementContainer/ManagementList';
-import { Autocomplete } from '@material-ui/lab';
-
+import {Autocomplete} from '@material-ui/lab';
 
 const useStyles = makeStyles((theme) => ({
-    mainContainer : {
-        paddingBottom:"vh",
+    mainContainer: {
+        paddingBottom: "vh",
         minHeight: "80vh",
         backgroundColor: "#deefff",
     },
@@ -32,23 +30,24 @@ const useStyles = makeStyles((theme) => ({
 
 function WelcomeView() {
     const classes = useStyles();
+
     return (
         <Grid container xs={12} className={classes.mainContainer}>
             <Grid item xs={12}>
                 <Grid container direction={"row"} className={classes.toolBar}>
                     <Grid xs={4}>
-                        <Button variant="outlined" >New product</Button>
+                        <Button variant="outlined">New product</Button>
                     </Grid>
-                   <Grid xs={5}>
-                       <TextField
-                           fullWidth
-                           name="search"
-                           margin="dense"
-                           variant="outlined"
-                           label="Search"
-                           placeholder= "Search a product"
-                       />
-                   </Grid>
+                    <Grid xs={5}>
+                        <TextField
+                            fullWidth
+                            name="search"
+                            margin="dense"
+                            variant="outlined"
+                            label="Search"
+                            placeholder="Search a product"
+                        />
+                    </Grid>
                 </Grid>
             </Grid>
             <Grid item xs={12} className={classes.displayContainer}>
