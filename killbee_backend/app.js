556df42aaa4f8f0a9ce532e47551ddb22d748e1a
Thibault <thibault@pop-os.localdomain>
2021-09-23 11:51:24 +0200

    updated app.js + deleted lemonldap

--- killbee_backend/app.js
+++ killbee_backend/app.js
@@ -1,16 +1,31 @@
 // Variables
 var express = require('express');
 var app = express();
-var handler = require('lemonldap-ng-handler');
+var ActiveDirectory = require('activedirectory');
+var config = {
+  url : '192.168.1.10', //mettre lien AD ici
+  basedn : 'dc=domain, dc=com',
+};
 
+var ad = new ActiveDirectory(config);
+var username = 'mettre utilisateur ici';
+var password = 'mettre MDP ici';
 
-// and load it
-app.use(handler.run);
-
-// Then simply use your express app
-app.get('/', function(req, res) {
-  return res.send('Hello ' + req.headers['Auth-User'] + ' !');
+ad.authenticate(username, password, function(err, auth) {
+    if (err) {
+        console.log('ERROR: '+JSON.stringify(err));
+        return;
+    }
+    if (auth) {
+        console.log('Authenticated!');
+    }
+    else {
+        console.log('Authentication failed!');
+    }
 });
-app.listen(3000, function() {
-  return console.log('Example app listening on port 3000!');
-});
+
+// // Then simply use your express app
+
+// app.listen(3000, function() {
+//   return console.log('Example app listening on port 3000!');
+// });
