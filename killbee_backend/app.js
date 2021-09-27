bcbad25a4c85034b0ef42db8ef12c3885103da05
Joseph LIAGRE <contact@josephliagre.com>
2021-09-27 09:41:46 +0200

    updated api, need to be tested on SERVER

--- killbee_backend/app.js
+++ killbee_backend/app.js
@@ -8,24 +8,49 @@ var ActiveDirectory = require('activedirectory');
 var config = require('./AD_config.json');
 
 var ad = new ActiveDirectory(config);
-var username = 'test@test.com';
-var password = '12345678';
+//var username = 'test@test.com';
+//var password = '12345678';
+var port = 3000
 
-ad.authenticate(username, password, function(err, auth) {
-    if (err) {
-        console.log('ERROR: '+JSON.stringify(err));
-        return;
-    }
-    if (auth) {
-        console.log('Authenticated!');
-    }
-    else {
-        console.log('Authentication failed!');
-    }
-});
+
 
 // // Then simply use your express app
 
-// app.listen(3000, function() {
-//   return console.log('Example app listening on port 3000!');
-// });  
+app.listen(port, function () {
+    return console.log('Started listening on port ' + port);
+});
+
+app.post('/auth', async (req, res) => {
+    //console.log(req.query);
+    try {
+
+        let auth_status = await auth(req.query.username, req.query.password)
+        let resp = await auth_status.json();
+        console.log(auth_status)
+        res.send(auth_status)
+    } catch {
+        res.send("SERVER ERROR")
+    }
+});
+
+
+async function auth(username, password) {
+
+    try {
+        ad.authenticate(username, password, function (err, auth) {
+            if (err) {
+                return "AUTH ERROR";
+            }
+            if (auth) {
+                return "ok"
+            }
+            else {
+                return "AUTH ERROR"
+            }
+        });
+    }
+    catch {
+        return "SERVER ERROR"
+    }
+
+}
