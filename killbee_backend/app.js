e13aac019eec7a38d49373f8303d34e1d7fb9eb3
Administrateur <administrateur@SRV-U20.04-DOCKER>
2021-09-28 16:13:23 +0200

    Feat : JWT token issuer working and retrieved correctly.

--- killbee_backend/app.js
+++ killbee_backend/app.js
@@ -1,55 +1,67 @@
-// Variables
 var express = require('express');
 var app = express();
 var ActiveDirectory = require('activedirectory');
 var cors = require('cors');
 var config = require('./AD_config.json');
-var bodyParser = require('body-parser');
 var ad = new ActiveDirectory(config);
-//var username = 'test@test.com';
-//var password = '12345678';
 var port = 3000;
 
 app.use(cors());
 
-// // Then simply use your express app
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
+
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const { json } = require('body-parser');
+dotenv.config();
+process.env.TOKEN_SECRET;
+
+
+
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
 
 app.listen(port, function () {
-    return console.log('Started listening on port ' + port);
+  return console.log('Started listening on port ' + port);
 });
 
 app.use(express.json({
-    type: ['application/json', 'text/plain']
-  }));
+  type: ['application/json', 'text/plain']
+}));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 
 app.post('/auth', async (req, res) => {
-    const  resserv="VIDE"
-    console.log(JSON.stringify(req.body));
-        await ad.authenticate(req.body.username, req.body.password, function (err, auth){
-       if (err) {
-                console.log(err)
-                console.log("1")
-                res.status(403);
-                    res.send("1")
-                }
-                else{
-                if (auth) {
-                console.log("2")
-                res.status(200);
-                res.send( JSON.stringify ("Ok"))
-                }
-                else {
-                console.log("3")
-                res.status(403);
-                res.send("AUTH ERROR")
-                }}})
-    
+  const resserv = "VIDE"
+  console.log(JSON.stringify(req.body));
+  await ad.authenticate(req.body.username, req.body.password, function (err, auth) {
+    if (err) {
+      console.log(err)
+      console.log("1")
+      res.status(403);
+      res.send("1")
+    }
+    else {
+      if (auth) {
+        console.log("2")
+        const token = generateAccessToken({ username: req.body.username });
+        res.json(token);
+        res.status(200);
+        res.send();
+      }
+      else {
+        console.log("3")
+        res.status(403);
+        res.send("AUTH ERROR")
+      }
+    }
+  })
+
 });
 
 
